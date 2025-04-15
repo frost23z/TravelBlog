45f383f12b45a8936301e77154d540c4affb0ea8
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-04-15 23:59:23 +0600

    feat: add NetworkError enum for handling various network error states

---
+++ app/src/main/java/me/zayedbinhasan/travelblog/util/NetworkError.kt
@@ -0,0 +1,15 @@
+package me.zayedbinhasan.travelblog.util
+
+enum class NetworkError : Error {
+    REQUEST_TIMEOUT,
+    UNAUTHORIZED,
+    CONFLICT,
+    TOO_MANY_REQUESTS,
+    NO_INTERNET,
+    PAYLOAD_TOO_LARGE,
+    SERVER_ERROR,
+    SERIALIZATION,
+    UNKNOWN,
+    NOT_FOUND,
+    FORBIDDEN;
+}
